23d0e48ec928818d68dcceeb8dedb49f3ce21b82
JunYinDM <jyin@g.harvard.edu>
2017-04-18 09:42:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ setup(name='NestedSampling_DMD',
       description='Tool for calculating rotational curve by DarkMatterDesensityFiles',
       url='https://github.com/p201-sp2016/NestedSampling_DMD',
       author='Daniya and Jun',
-      author_email='yinjun02@gmail.com',
+      author_email='yinjun02@gmail.com', 'daniya_seitova@g.harvard.edu ',
       license='GNU General Public License v3.0',
       packages=['NestedSampling_DMD'],
       install_requires=[
           'numpy',
-          'nestle'
+          'nestle',
+          'math',
+          'corner',
+          'matplotlib',
+          'scipy'
+            
       ],
       zip_safe=False,
       test_suite='nose.collector',
