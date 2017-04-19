70927e8609170ace8e38690779374a66ba3e04d1
Jun Yin <jyin@g.harvard.edu>
2017-04-19 04:25:10 -0400

    lala

--- textdmdcode.py
+++ textdmdcode.py
@@ -22,21 +22,33 @@ class TestFunctions(unittest.TestCase):
         theta0_range = np.linspace(0,0.1,3)
         theta1_range = np.linspace(0,0.1,3)
         priorRange=np.ones((len(theta0_range),len(theta1_range)))
+        
 
         for i in range(len(theta0_range)):
             for j in range(len(theta1_range)):
                 
-                theta = theta0_range, theta1_range 
+                theta = theta0_range[i], theta1_range[j]
 
                 priorRange[i,j]= np.argmax(model.prior_transform_NFW(theta))
             
         self.assertTrue(np.argmax(priorRange)<10000 and np.argmax(priorRange)>-1)
         
-        
 
         
         
-
+     
+    def test_model_NFW(self):
+        
+        theta0_range = np.linspace(6,1,10)
+        vrot=np.ones(len(theta0_range))
+        
+        for i in range(len(theta0_range)):
+            
+            theta = theta0_range[i], 10
+            vrot[i]=model.model_NFW(theta,11)
+                    
+                    
+        self.assertTrue(np.argmax(vrot)>-1000)
         
 
 
