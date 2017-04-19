7afcab1d71eda12216bccb6710fec9fd78ac630c
Jun Yin <jyin@g.harvard.edu>
2017-04-19 04:02:53 -0400

    lala

--- textdmdcode.py
+++ textdmdcode.py
@@ -1,68 +1,46 @@
 import unittest
+import os
+import numpy as np
 
 
 from nsdmd import model
 from nsdmd import dmdio
 
-import os
 
 
 
 class TestFunctions(unittest.TestCase):
     def test_io(self):
         filename = 'example_data.txt'
-        testdata = dmdio.load_data(filename)
-        self.assertTrue(issubclass(type(testdata), np.ndarray))
+        testdata_x,testdata_xerr,testdata_y,testdata_yerr = dmdio.load_data(filename)
+        self.assertTrue(issubclass(type(testdata_x), np.ndarray))
         
         
-        
-    def test_model_NFW(self):
-        
-        theta_range = np.linspace(0,0.1,3)
-        
-        for i in rannge(len(theta_range)):
-            vrot[i]=model.model_NFW(theta,x)
-        self.assertTrue(np.argmax(vrot)>0 and np.argmax(vrot) <1000)
-        
-        
-        
-        
-    def test_loglike_NFW(self):
-                
-        theta0_range = np.linspace(0,0.1,3)
-        theta1_range = np.linspace(0,0.1,3)
-
-
-        for i in rannge(len(theta0_range)):
-            for j in rannge(len(theta1_range)):
-                
-                theta = theta0, theta1 
-                minusChiSq = model.prior_loglike_NFW(theta)
-                
-        self.assertTrue(np.argmax(minusChiSq)<0)
-        
-        
-
         
     def test_prior_transform_NFW(self):
         testdata = dmdio.load_data('example_data.txt')
         theta0_range = np.linspace(0,0.1,3)
         theta1_range = np.linspace(0,0.1,3)
+        priorRange=np.ones((len(theta0_range),len(theta1_range)))
 
-
-        for i in rannge(len(theta0_range)):
-            for j in rannge(len(theta1_range)):
+        for i in range(len(theta0_range)):
+            for j in range(len(theta1_range)):
                 
-                theta = theta0, theta1 
+                theta = theta0_range, theta1_range 
 
-                priorRange= model.prior_transform_NFW(theta)
+                priorRange[i,j]= np.argmax(model.prior_transform_NFW(theta))
             
-        self.assertTrue(np.argmax(priorRange)<1000 and np.argmax(priorRange)>1000 )
+        self.assertTrue(np.argmax(priorRange)<10000 and np.argmax(priorRange)>-1)
+        
+        
+
+        
+        
+
+        
 
 
+
+if __name__ == '__main__':
+    unittest.main()            
             
-            
-
-
-
-print("testcode works fine")
