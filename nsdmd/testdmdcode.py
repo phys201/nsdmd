10c220b4fda28e05c03f260e800d413f9d1f1f25
JunYinDM <jyin@g.harvard.edu>
2017-04-30 21:24:14 -0400

    test code updated

--- nsdmd/testdmdcode.py
+++ nsdmd/testdmdcode.py
@@ -8,18 +8,31 @@ from nsdmd import io
 
 
 class TestFunctions(unittest.TestCase):
+    
+    # test data open function 
     def test_io(self):
+        
+        # import data 
         filename = 'dataref1.txt'
         testdata_x,testdata_xerr,testdata_y,testdata_yerr = io.load_data(filename)
+        
+        
         self.assertTrue(issubclass(type(testdata_x), np.ndarray))
         
+        
+        
+    # test prior function 
     def test_prior_transform_NFW(self):
+        
+        # open data 
         testdata = io.load_data('dataref1.txt')
+        
+        # limit the range of parameters 
         theta0_range = np.linspace(0,0.1,3)
         theta1_range = np.linspace(0,0.1,3)
         priorRange=np.ones((len(theta0_range),len(theta1_range)))
         
-
+        # generating priors 
         for i in range(len(theta0_range)):
             for j in range(len(theta1_range)):
                 
@@ -29,52 +42,59 @@ class TestFunctions(unittest.TestCase):
             
         self.assertTrue(np.argmax(priorRange)<10000 and np.argmax(priorRange)>-1)        
         
-        
+    # test model    
     def test_model_NFW(self):
         
         theta0_range = np.linspace(6,1,10)
         vrot=np.ones(len(theta0_range))
         
+        # calculte velocities by the model given mass density function 
         for i in range(len(theta0_range)):
             
             theta = theta0_range[i], 10
             vrot[i]=model.model_NFW(theta,11)
-                    
+        
+        # test velocity 
         self.assertTrue(np.argmax(vrot)>-1000)
              
         
-        
+    # test model    
     def test_model_ISO(self):
         
         theta0_range = np.linspace(6,1,10)
         vrot=np.ones(len(theta0_range))
         
+        # calculte velocities by the model given mass density function 
         for i in range(len(theta0_range)):
-            
+           
             theta = theta0_range[i], 10
             vrot[i]=model.model_ISO(theta,11)
-                    
+        
+        #test velocities 
         self.assertTrue(np.argmax(vrot)>-1000)
                      
-        
+    # test model     
     def test_model_Einasto(self):
         
         theta0_range = np.linspace(6,1,10)
         vrot=np.ones(len(theta0_range))
         
+        # calculte velocities by the model given mass density function 
         for i in range(len(theta0_range)):
             
             theta = theta0_range[i], 10, 50
             vrot[i]=model.model_Einasto(theta,11)
-                    
+            
+        # test velocities             
         self.assertTrue(np.argmax(vrot)>-1000)
         
         
         
         
-        
+    # test nestle sampling     
     def test_sample(self):
         
+        # testing the result on the NFW model 
         result = model.sample (model.loglike_NFW, model.prior_transform_NFW, 'example_data.txt',[10,500])
         
         self.assertTrue(isinstance(result.logz, float))
